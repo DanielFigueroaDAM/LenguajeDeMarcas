a5d4a51ef3b6a90048dce2c61c0c8a2ece51b074
Figueroa <dfigueroavidal@danielcastelao.org>
2025-03-12 13:09:58 +0100

    empezando con css

---
+++ AjedrezCSS/para_JavaScript/primer/holamundo.js
@@ -0,0 +1,29 @@
+console.log("Hola mundo") //si recibe numeros por pantalla no se convierten en tipo number, siempre se convierten en string
+//para solucionar el problema de casteo se usaria:
+let number=  parseInt(console.log("Dime el numero"))
+prompt("inserte algo")
+alert("Ojo cuidao")
+
+let edad=16.5 //variable local
+var  juegoFavorito="Red Dead" //variable general
+const nombre="Daniel" //valores que no cambian
+
+let bol=true; // true o false
+
+
+//comparacion simple
+
+// 2=="2" // esto recibe true
+//=!
+//comparacion estricta
+
+// 2==="2" // esto recibe false
+//!==
+
+
+// 2**2 //potencia
+
+// las funciones se escriben
+//function sumaNumeros(num1,num2){
+//return num1+num2 // no es obligatorio el return
+//}
