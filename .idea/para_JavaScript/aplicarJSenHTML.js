f1a5e71908151d530d0d3e731ed34c4c3ba7a54a
Figueroa <dfigueroavidal@danielcastelao.org>
2025-03-19 13:11:22 +0100

    generador de excusas sin decorar

--- .idea/para_JavaScript/aplicarJSenHTML.js
+++ .idea/para_JavaScript/aplicarJSenHTML.js
@@ -1,4 +1,9 @@
-let mih1=document.querySelector("h1")
-mih1.innerHTML("Adios buenas noches")
+let mih1 = document.querySelector("h1");
+mih1.innerHTML = "Adios buenas noches";
+console.log(mih1);
 
-console.log(mih1)
+function cambiarColor(){
+    mih1.style.color="green"
+}
+
+mih1.addEventListener("click",cambiarColor)
---
+++ .idea/para_JavaScript/generadorDeExcusas.js
@@ -0,0 +1,61 @@
+let mih1 = document.querySelector("h1");
+
+mih1.addEventListener("click",cambiarTexto)
+
+function cambiarTexto(){
+    mih1.innerHTML=quien()+" "+" "+que()+" "+" "+como()+" "+" "+palabraFinal()
+}
+
+function quien() {
+    let opciones = [
+        "Mi hermano menor",
+        "El perro de mi vecina",
+        "Mi compañero de cuarto",
+        "El repartidor de pizza",
+        "Mi primo recién llegado",
+        "Mi profesor de matemáticas",
+        "El vigilante de la entrada"
+      ];
+    return randomItem(opciones);
+  }
+  
+  function que() {
+    let opciones = [
+        "tomó mis tareas",
+        "tiró mis tareas por la ventana",
+        "estornudó sobre mis tareas",
+        "arrugó mis tareas",
+        "pisoteó mis tareas",
+        "quemó mis tareas"
+      ];
+    return randomItem(opciones);
+  }
+  
+  function como() {
+    let opciones = [
+        "sin darse cuenta",
+        "mientras jugaba al Fortnite",
+        "cuando intentaba ayudar",
+        "gritando de pánico",
+        "mientras grababa un TikTok",
+        "con cara de pocos amigos",
+        "corriendo como loco"
+      ];
+    return randomItem(opciones);
+  }
+  
+  function palabraFinal() {
+    let opciones = [
+        "y tuve que empezar de cero.",
+        "y todo se arruinó por completo.",
+        "y desapareció sin dejar rastro.",
+        "y ya no pude presentarlas a tiempo.",
+        "y nunca logré recuperarlas.",
+        "y terminé metido en un gran lío."
+      ];
+    return randomItem(opciones);
+  }
+
+  function randomItem(array){
+        return array[(Math.floor(Math.random()*array.length))];
+  }
