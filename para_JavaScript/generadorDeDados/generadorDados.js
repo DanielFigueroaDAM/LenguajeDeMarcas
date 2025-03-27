33e80eb27858fd48dc987b3f7e81fda3c92087e6
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-03-27 17:36:49 +0100

    dados casi terminado

--- para_JavaScript/generadorDeDados/generadorDados.js
+++ para_JavaScript/generadorDeDados/generadorDados.js
@@ -2,22 +2,34 @@ const contenedor = document.getElementById('contenedor');
 let boton = document.querySelector("#gBtn");
 var cantidad = document.querySelector("#cantidad");
 
+var modSuma=document.querySelector("#suma")
+
+var suma=0;
+
 
 boton.addEventListener("click", tirarDados);
 
 function tirarDados(){
   contenedor.innerHTML = '';
+  suma=0;
   const cantidadImagenes = parseInt(cantidad.value);
   for (let i = 1; i <= cantidadImagenes; i++) {
     generarDado()
   }
+  modSuma.innerHTML=suma;
 }
 
 function generarDado(){
   let num=randomItem();
   contenedor.innerHTML += `<img id="dado_generado" src="dado${num}.png" alt="Dado ${num}" width="300">`;
+
 }
 
 function randomItem(){
-  return Math.floor(Math.random() * 6) + 1;
+  let randomz= Math.floor(Math.random() * 6) + 1;
+  suma=suma+randomz;
+  return randomz
+}
+function mostrarSuma(){
+  return suma;
 }
