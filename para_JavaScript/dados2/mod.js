00b5b809a49c6ae2a71f95a898218099ca7a6cac
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-04-02 21:27:43 +0200

    programa de generar un dado

---
+++ para_JavaScript/dados2/mod.js
@@ -0,0 +1,27 @@
+var dado = document.querySelector(".espacio")
+let boton = document.querySelector(".gBoton")
+
+boton.addEventListener("click", tiraDado)
+
+
+
+
+function tiraDado(){
+    let num=randomItem();
+    dado.innerHTML=`<img id="dado_generado" src="dado${num}.png" width="300">`;
+}
+
+
+
+
+
+
+
+
+
+
+
+function randomItem(){
+    let randomz= Math.floor(Math.random() * 6) + 1;
+    return randomz
+  }
