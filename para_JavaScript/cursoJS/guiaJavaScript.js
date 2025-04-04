45d91a3503bf8d94d43737b1e4c8b987ccae1966
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-04-04 18:02:56 +0200

    avanzando en el curso java con funciones

--- para_JavaScript/cursoJS/guiaJavaScript.js
+++ para_JavaScript/cursoJS/guiaJavaScript.js
@@ -14,3 +14,20 @@ return copyright;
 (function(){
     alert("Hola soy una funcion autoinvocada")
 })();
+//Otra forma de hacer una funcion autoinvocada
+(function(name,year){
+    console.log(name,year)
+}("Juan",2023));
+//Otra forma de hacer una funcion autoinvocada
+(function(name="Juan",year=2023){
+    console.log(name,year)
+}());
+
+
+//Ejemplo de uso de la funcion getCopyright con una variable ya definida
+
+function getCopyright2(name,year=2023){
+    return `Copyright ${year} ${name}`;
+}
+console.log(getCopyright2("Juan")); //Esto funciona y se agarra el año ya definido en la funcion
+console.log(getCopyright2("Juan",2025)); //Esto funciona y se agarra el año que le pasamos como argumento
