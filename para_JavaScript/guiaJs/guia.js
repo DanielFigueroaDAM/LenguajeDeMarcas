606a0ff62aaded05ffc080de3429c3e0faa143f0
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-04-25 20:46:31 +0200

    empezando la guia de js

---
+++ para_JavaScript/guiaJs/guia.js
@@ -0,0 +1,21 @@
+//Sacar número random
+function numeroRandom() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+//Comprobar el número
+function reiniciarSitioWeb() {
+  setTimeout(() => {
+    location.reload();
+  }, 2000);
+}
+//recoger datos por un imput
+function recogerDatos() {
+  const input = document.querySelector(".input");
+  const datoIngresado = document.getElementById("nombre").value;
+  console.log(datoIngresado);
+  // si quieres vacial lo que hay en el imput
+    input.value = "";
+
+}
+let btn=document.getElementById("btn");
+btn.addEventListener("click", recogerDatos);
