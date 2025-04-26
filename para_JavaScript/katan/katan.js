f906910c228362be4c98a762a07988e82a678c3c
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-04-26 14:18:26 +0200

    haciendo array de objetos del campo del katan

---
+++ para_JavaScript/katan/katan.js
@@ -0,0 +1,91 @@
+let mapaKatan=[{
+    id: 1,
+    tipo: "pedra",
+    numero:10,
+},
+{
+    id: 2,
+    tipo: "ovella",
+    numero: 2,
+},
+{
+    id: 3,
+    tipo: "madeira",
+    numero: 9,
+},
+{
+    id: 4,
+    tipo: "trigo",
+    numero: 12,
+},
+{
+    id: 5,
+    tipo: "barro",
+    numero: 6,
+},
+{
+    id: 6,
+    tipo: "ovella",
+    numero: 4,
+},
+{
+    id: 7,
+    tipo: "barro",
+    numero: 10,
+},
+{
+    id: 8,
+    tipo: "trigo",
+    numero: 9,
+},
+{
+    id: 9,
+    tipo: "madeira",
+    numero: 11,
+}
+,{
+    id: 10,
+    tipo: "madeira",
+    numero: 3,
+},
+{
+    id: 11,
+    tipo: "pedra",
+    numero: 8,
+},
+{
+    id: 12,
+    tipo: "madeira",
+    numero: 8,
+},
+{
+    id: 13,
+    tipo: "pedra",
+    numero: 3,
+},
+{
+    id: 14,
+    tipo: "trigo",
+    numero: 4,
+},
+{
+    id: 15,
+    tipo: "ovella",
+    numero: 5,
+},
+{
+    id: 16,
+    tipo: "barro",
+    numero: 5,
+},
+{
+    id: 17,
+    tipo: "trigo",
+    numero: 6,
+},
+{
+    id: 18,
+    tipo: "ovella",
+    numero: 11,
+}
+]
